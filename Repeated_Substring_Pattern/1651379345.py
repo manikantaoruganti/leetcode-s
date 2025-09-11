3023dc90c6c3c361e0b916e019d4f228b2e865e0
Manikanta <orugantimanikanta24@gmail.com>
2025-09-11 18:31:23 +0530

    Add solution: Repeated_Substring_Pattern

---
+++ Repeated_Substring_Pattern/1651379345.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def repeatedSubstringPattern(self, s):
+        return s in (s + s)[1:-1]
+        """
+        :type s: str
+        :rtype: bool
+        """
+        
