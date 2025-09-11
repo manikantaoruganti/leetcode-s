c4220528f4967fb99c1af932af2b5aeb848f6bb4
Manikanta <orugantimanikanta24@gmail.com>
2025-09-11 18:29:36 +0530

    Add solution: Find_the_Middle_Index_in_Array

---
+++ Find_the_Middle_Index_in_Array/1650369499.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int findMiddleIndex(vector<int>& nums) {
+        int total = accumulate(nums.begin(), nums.end(), 0), left = 0;
+for (int i = 0; i < nums.size(); ++i) {
+    if (left == total - left - nums[i]) return i;
+    left += nums[i];
+}
+return -1;
+        
+    }
+};
