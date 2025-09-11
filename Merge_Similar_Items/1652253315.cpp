3e2b0b4d05266c87cbae6a2edb48d9f610ba3343
Manikanta <orugantimanikanta24@gmail.com>
2025-09-11 18:30:32 +0530

    Add solution: Merge_Similar_Items

---
+++ Merge_Similar_Items/1652253315.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> mergeSimilarItems(vector<vector<int>>& items1, vector<vector<int>>& items2) {
+       map<int, int> weightMap;
+
+for (auto &item : items1)
+    weightMap[item[0]] += item[1];
+
+for (auto &item : items2)
+    weightMap[item[0]] += item[1];
+
+vector<vector<int>> result;
+for (auto &[value, weight] : weightMap)
+    result.push_back({value, weight});
+
+return result;
+        
+    }
+};
