6c1b41a950dc5b71bf38750f29481092846e6e0a
Manikanta <orugantimanikanta24@gmail.com>
2025-09-11 18:30:41 +0530

    Add solution: Minimum_Operations_to_Exceed_Threshold_Value_I

---
+++ Minimum_Operations_to_Exceed_Threshold_Value_I/1618183486.cpp
@@ -0,0 +1,11 @@
+class Solution {
+ public:
+  int minOperations(vector<int>& nums, int k) {
+   
+      
+      
+      
+      return ranges::count_if(nums, [k](int num)
+                              { return num < k; });
+  }
+};
