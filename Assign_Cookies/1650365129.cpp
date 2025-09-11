f9b2795ba9218e5a00f049228953d08c863ff509
Manikanta <orugantimanikanta24@gmail.com>
2025-09-11 18:28:25 +0530

    Add solution: Assign_Cookies

---
+++ Assign_Cookies/1650365129.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+      std::sort(g.begin(), g.end());
+        std::sort(s.begin(), s.end());
+        int i = 0, j = 0;
+        while (i < g.size() && j < s.size()) {
+            if (s[j] >= g[i]) i++;
+            j++;
+        }
+        return i;  
+    }
+};
