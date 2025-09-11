9af94d3150ff3d22da40bab1073ea5fb2f8b3371
Manikanta <orugantimanikanta24@gmail.com>
2025-09-11 18:29:10 +0530

    Add solution: Design_Circular_Deque

---
+++ Design_Circular_Deque/1621203246.cpp
@@ -0,0 +1,82 @@
+class MyCircularDeque {
+public:
+    /** Initialize your data structure here. Set the size of the deque to be k. */
+    MyCircularDeque(int k) : capacity(k), deque(k), front(0), back(k - 1), currentSize(0) {}
+
+    /** Adds an item at the front of Deque. Return true if the operation is successful. */
+    bool insertFront(int value) {
+        if (isFull()) return false;
+        
+        front = (front - 1 + capacity) % capacity;
+        deque[front] = value;
+        currentSize++;
+        return true;
+    }
+
+    /** Adds an item at the rear of Deque. Return true if the operation is successful. */
+    bool insertLast(int value) {
+        if (isFull()) return false;
+
+        back = (back + 1) % capacity;
+        deque[back] = value;
+        currentSize++;
+        return true;
+    }
+
+    /** Deletes an item from the front of Deque. Return true if the operation is successful. */
+    bool deleteFront() {
+        if (isEmpty()) return false;
+        
+        front = (front + 1) % capacity;
+        currentSize--;
+        return true;
+    }
+
+    /** Deletes an item from the rear of Deque. Return true if the operation is successful. */
+    bool deleteLast() {
+        if (isEmpty()) return false;
+
+        back = (back - 1 + capacity) % capacity;
+        currentSize--;
+        return true;
+    }
+
+    /** Get the front item from the deque. */
+    int getFront() {
+        return isEmpty() ? -1 : deque[front];
+    }
+
+    /** Get the last item from the deque. */
+    int getRear() {
+        return isEmpty() ? -1 : deque[back];
+    }
+
+    /** Checks whether the circular deque is empty or not. */
+    bool isEmpty() {
+        return currentSize == 0;
+    }
+
+    /** Checks whether the circular deque is full or not. */
+    bool isFull() {
+        return currentSize == capacity;
+    }
+
+private:
+    const int capacity;
+    vector<int> deque;
+    int front, back, currentSize;
+};
+
+
+/**
+ * Your MyCircularDeque object will be instantiated and called as such:
+ * MyCircularDeque* obj = new MyCircularDeque(k);
+ * bool param_1 = obj->insertFront(value);
+ * bool param_2 = obj->insertLast(value);
+ * bool param_3 = obj->deleteFront();
+ * bool param_4 = obj->deleteLast();
+ * int param_5 = obj->getFront();
+ * int param_6 = obj->getRear();
+ * bool param_7 = obj->isEmpty();
+ * bool param_8 = obj->isFull();
+ */
