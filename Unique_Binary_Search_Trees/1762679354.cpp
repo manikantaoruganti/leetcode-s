dd221f6553ae83ae3fab6fb894541374be9173f1
Manikanta <orugantimanikanta24@gmail.com>
2025-09-11 18:32:07 +0530

    Add solution: Unique_Binary_Search_Trees

---
+++ Unique_Binary_Search_Trees/1762679354.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int numTrees(int n) {
+        long long result=1;
+        for(int i=0;i<n;i++){
+            result*=2*n-i;
+            result/=i+1;}
+            result/=n+1;
+                
+            
+         return result;
+    }
+};
