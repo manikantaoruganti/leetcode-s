d8d2ea8567c3bb7adb25a4786477a3e0da6c6d46
Manikanta <orugantimanikanta24@gmail.com>
2025-09-11 18:30:18 +0530

    Add solution: Maximum_Ascending_Subarray_Sum

---
+++ Maximum_Ascending_Subarray_Sum/1651630809.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxAscendingSum(vector<int>& nums) {
+       int maxSum = nums[0], currSum = nums[0];
+for (int i = 1; i < nums.size(); ++i) {
+    if (nums[i] > nums[i - 1]) currSum += nums[i];
+    else currSum = nums[i];
+    if (currSum > maxSum) maxSum = currSum;
+}
+return maxSum;
+        
+    }
+};
