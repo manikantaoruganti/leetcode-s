ec8b073d289fbe699404a257bd1f259c56574f35
Manikanta <orugantimanikanta24@gmail.com>
2025-09-11 18:30:33 +0530

    Add solution: Merge_Sorted_Array

---
+++ Merge_Sorted_Array/1580287114.rs
@@ -0,0 +1,23 @@
+impl Solution {
+    pub fn merge(nums1: &mut Vec<i32>, m: i32, nums2: &mut Vec<i32>, n: i32) {
+        let (mut i, mut j, mut k) = (m as isize - 1, n as isize - 1, (m + n) as isize - 1);
+        
+        while i >= 0 && j >= 0 {
+            if nums1[i as usize] > nums2[j as usize] {
+                nums1[k as usize] = nums1[i as usize];
+                i -= 1;
+            } else {
+                nums1[k as usize] = nums2[j as usize];
+                j -= 1;
+            }
+            k -= 1;
+        }
+
+        // If nums2 has remaining elements, copy them to nums1
+        while j >= 0 {
+            nums1[k as usize] = nums2[j as usize];
+            j -= 1;
+            k -= 1;
+        }
+    }
+}
