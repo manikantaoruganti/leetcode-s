5831b3ee933cd1aa061561216f85e5dd0bf398d6
Manikanta <orugantimanikanta24@gmail.com>
2025-09-11 18:29:19 +0530

    Add solution: Element_Appearing_More_Than_25__In_Sorted_Array

---
+++ Element_Appearing_More_Than_25__In_Sorted_Array/1618212863.cpp
@@ -0,0 +1,25 @@
+/*class Solution {
+public:
+    int findSpecialInteger(vector<int>& arr) {
+        
+    }
+};*/
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int findSpecialInteger(vector<int>& arr) {
+        int n = arr.size();
+        int count = 1;
+        for (int i = 1; i < n; i++) {
+            if (arr[i] == arr[i - 1]) {
+                count++;
+                if (count > n / 4) return arr[i];
+            } else {
+                count = 1;
+            }
+        }
+        return arr[0]; // Only to satisfy compiler; logically always returned above
+    }
+};
