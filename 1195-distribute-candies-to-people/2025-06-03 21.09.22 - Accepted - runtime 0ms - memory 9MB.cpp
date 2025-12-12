a2f8f52b531555a8a30772c48326716d0f5e3797
manikanta <155448771+manikantaoruganti@users.noreply.github.com>
2025-12-12 09:42:40 +0530

    Update 2025-06-03 21.09.22 - Accepted - runtime 0ms - memory 9MB.cpp

--- 1195-distribute-candies-to-people/2025-06-03 21.09.22 - Accepted - runtime 0ms - memory 9MB.cpp
+++ 1195-distribute-candies-to-people/2025-06-03 21.09.22 - Accepted - runtime 0ms - memory 9MB.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
-    vector<int> distributeCandies(int candies, int num_people) {
-        
+    vector<int> distributeCandies(int candies, int num_people){
     vector<int> ans(num_people, 0);
         int give = 1, i = 0;
         while (candies > 0) {
