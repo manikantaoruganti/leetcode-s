babe937971f3a453c5d5a30e247ff4c657ca0593
manikanta <155448771+manikantaoruganti@users.noreply.github.com>
2026-04-10 21:08:03 +0530

    Update

--- 1704-special-positions-in-a-binary-matrix/2025-06-02 16.41.29 - Accepted - runtime 0ms - memory 17.1MB.cpp
+++ 1704-special-positions-in-a-binary-matrix/2025-06-02 16.41.29 - Accepted - runtime 0ms - memory 17.1MB.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int numSpecial(vector<vector<int>>& mat) {
-       int m = mat.size(), n = mat[0].size(), res = 0;
+    int m = mat.size(), n = mat[0].size(), res = 0;
 vector<int> rowSum(m, 0), colSum(n, 0);
 for (int i = 0; i < m; ++i)
     for (int j = 0; j < n; ++j) {
